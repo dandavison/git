5962604c947ea6621f2f939ee27be1748cb386ea
Dan Davison <dandavison7@gmail.com>
2020-05-10 18:51:48 -0400

    Allow git prompt characters to be customized

--- contrib/completion/git-prompt.sh
+++ contrib/completion/git-prompt.sh
@@ -505,8 +505,8 @@ __git_ps1 ()
 		if [ -n "${GIT_PS1_SHOWDIRTYSTATE-}" ] &&
 		   [ "$(git config --bool bash.showDirtyState)" != "false" ]
 		then
-			git diff --no-ext-diff --quiet || w="*"
-			git diff --no-ext-diff --cached --quiet || i="+"
+			git diff --no-ext-diff --quiet || w="${GIT_PS1_UNSTAGED:-*}"
+			git diff --no-ext-diff --cached --quiet || i="${GIT_PS1_STAGED:-+}"
 			if [ -z "$short_sha" ] && [ -z "$i" ]; then
 				i="#"
 			fi
